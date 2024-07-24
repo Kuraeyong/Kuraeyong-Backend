1c8c192442511ef3f0eba5229ef8669985cc386b
Minseok <saltishDev@gmail.com>
2024-07-24 15:05:17 +0900

    Test: JDBCTemplate 적용

--- src/main/java/kuraeyong/backend/dao/StationDao.java
+++ src/main/java/kuraeyong/backend/dao/StationDao.java
@@ -3,23 +3,29 @@ package kuraeyong.backend.dao;
 import kuraeyong.backend.dto.GetListResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.util.*;
 
 @Slf4j
 @Repository
-@RequiredArgsConstructor
 public class StationDao {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public StationDao(DataSource dataSource) {
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
     public GetListResponse getLineNameListByStationName(String stationName) {
         log.info("[StationDao.getLineNameListByStationName]");
 
-        // 임시 코드
-        List<String> list = new ArrayList<>();
-        list.add("2");
-        list.add("5");
-        list.add("경의중앙");
-        list.add("수인분당");
+        String sql = "select line from station where name=:name";
+        Map<String, Object> param = Map.of("name", stationName);
+
+        List<String> list = jdbcTemplate.query(sql, param, (rs, count) -> rs.getString("line"));
 
         return new GetListResponse(list);
     }
